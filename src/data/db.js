f8147ab7e7fa72bd1f59fd12cc25cc87aecde598
Audie Artavia <aj.artavia19@gmail.com>
2022-09-25 21:28:25 +0200

    updated text

--- src/data/db.js
+++ src/data/db.js
@@ -73,7 +73,7 @@ export const experience = [
         company: 'Softtek',
         start: '09/2021',
         end: '06/2022',
-        description: 'RPA development using C# and Selenium Overseeing bot servers status through custom software and MSSQL to ensure they function as expected.\n Build PowerBI reports to extract bot server information also, deliver real time analytical data to our       clients, providing insight on orders.\n Communication with clients to make sure the automated solutions are meeting their efficiency expectations while acurately handling the orders.',
+        description: 'RPA development using C# and Selenium Overseeing bot servers status through custom software and MSSQL to ensure they function as expected.\n Build PowerBI reports to extract bot server information also, deliver real time analytical data to our clients, providing insight on orders.\n Communication with clients to make sure the automated solutions are meeting their efficiency expectations while acurately handling the orders.',
     },
     {
         company: 'Bosch',
@@ -94,27 +94,27 @@ export const projects = [
         name: "python api",
         img: null,
         desc: "Api for listing movies and restaurants",
-        tech: ["python", "api stuff"],
+        tech: ["Python", "Flask", "MongoDB", "Docker"],
         git: "https://github.com/audieartb/FMR-API"
     },
     {
         name: "TCU",
         img: null,
         desc: "administration and enrollment of students at the Tinoco School Project for community schoolarization",
-        tech: ["vueJs", "GraphQL", "NodeJs", "MongoDB" ],
+        tech: ["VueJs", "GraphQL", "NodeJs", "MongoDB" ],
         git: ""
     },
     {
         name: "ME2",
         img: null,
-        desc: "University project for real lide application, this project was developed for Mercadeo en Linea S.A, a marketing and developement company from Costa Rica",
-        tech: ["VueJs", "MongoDb", "NodeJs"],
+        desc: "University project for real life use, this project was developed for Mercadeo en Linea S.A, a marketing and developement company from Costa Rica",
+        tech: ["VueJs", "MongoDB", "NodeJs", "Ubuntu", "Nginx"],
         git: ""
     },
     {
         name: "My Portfolio",
         img: null,
-        desc: "portfolio to keep the projects I work on available for recruiters",
+        desc: "This project was made using a simple approach with javascript, html and css",
         tech: ["Javascript", "Html/Css", "Jquery", "Webpack"],
         git: "https://github.com/audieartb/myportfolio/tree/master",
     }
